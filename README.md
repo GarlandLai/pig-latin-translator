feba824e01bfc14d1847dfacb0778845e3c3157a
Cord Walsh and Garland Lai <cord@>
2019-06-12 15:54:43 -0700

    add semi-functional pig latin translator (single vowels, consonants, y value, and 2 consonants)

---
+++ README.md
@@ -0,0 +1,50 @@
+# Garland Lai and Cord Walsh Program Language Form
+
+### _Epicodus JavaScript and JQuery Project for Week 2, June 7th, 2019_
+
+### By _*Garland Lai*_
+
+## Description
+
+_This page was created to help users pick which program language to learn. It was use JavaScript and JQuery  lessons in week two's curriculum._
+
+## Setup/Installation Requirements
+
+Behaviors	Input                 |    Examples	                 | Output Examples
+Our program should handle	      |    When it receives:	         | It should return:
+_--------------------------------------------------------------------------------_
+Not a letter                    |  	123	                       | Please type a word     
+Beginning with a vowel	        |   apple	                     | appleay
+Multiple words sentence starting with vowels		
+Begins with 1 or more consonants|	  speak	                     | eakspay
+Multiple words sentence starting with consonants		
+Begins with a "y", treat like a consonant|  	yelp	           |elpyay
+Begins with "qu" move both 	    |   Quiet	                     | ietquay
+
+2. _Clone the repository._
+3. _Save to desired location._
+4. _Open the index_html file with your preferred web browser._
+5. _Fill out the form and see what language suits you best!_
+
+## Known Bugs
+
+*
+
+
+## Support and contact details
+
+
+## Technologies Used
+
+* _Github_
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _JavaScript_
+* _JQuery_
+
+#### License
+
+*This webpage is licensed under the MIT license.*
+
+Copyright (c) 2019 **_Cord Walsh & Garland Lai_**
---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+// sets up the document as ready
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O','U','y','Y']
+    var consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'W','Y','Z','b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'w', 'y', 'z']
+    var userInput = ($("#input1").val());
+    var qu = ['qu', 'QU', 'Qu']
+
+
+    for (var index = 0; index < userInput.length; index += 1)
+
+        if (userInput[0] === 'y'|| userInput[0] === 'Y') {
+          var vowelSlice = userInput.slice(1)
+          $("#output").text(vowelSlice + userInput[0] + "ay");
+        }
+        else if (vowels.includes(userInput[0])) {
+        $("#output").text(userInput + "way");
+        }
+        else if (qu.includes(userInput[""])) {
+          var word = userInput.slice(this)
+          $("#output").text(userInput.push(word) + "ay")
+        }
+      else if (consonants.includes(userInput[0]) && consonants.includes(userInput[1])) {
+      var consonantSlice = userInput.slice(2)
+      $("#output").text(consonantSlice + userInput[0] + userInput[1] + "ay");
+      }
+        else if (consonants.includes(userInput[0])) {
+        var consonantSlice = userInput.slice(1)
+        $("#output").text(consonantSlice + userInput[0] + "ay");
+      }
+
+          else {
+         $("#output").text("sorry dawg");
+        }
+
+        // // else if (
+        // //   for (var index = 0; index <= ; index +=1)
+        // // )
+        //
+        //
+        // (consonants.includes(userInput[0])) {
+        // var multipleCons = userInput.slice(vowels)
+        // $("#output").text(multipleCons + userInput[0] + "ay");
+        // }
+      // };
+    console.log(userInput)
+  });
+  });
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//     $(".formOne").submit(function(event) {
+//       var vowels = ['a', 'e', 'i', 'o', 'u'];
+//       var userInput = $("#input1").val()).split("");
+//       // var userInput = ($("#input1").val()).split("");
+//       console.log(userInput);
+//       event.preventDefault();
+//     });
+//   });
+
+
+      // if (userInput[0] === "a" || userInput[0] === "e" || userInput[0] === "i" || userInput[0] === "o" || userInput[0] === "u") {
